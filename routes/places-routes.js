12997a573764fa24bb3788dea33474ce374c9462
Labranche <slabranche@cmontmorency.qc.ca>
2023-03-08 08:38:43 -0500

    Premiere route

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -4,4 +4,7 @@ const router = express.Router();
 
 router.get('/', (requete, reponse, next) => {
     console.log("Requête GET dans places-routes")
+    reponse.json({message: "Ça fonctionne!"});
 });
+
+module.exports = router;
