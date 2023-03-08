f0e6f162d496f52fe6699151d1ad3d2faf3a2caf
Labranche <slabranche@cmontmorency.qc.ca>
2023-03-08 10:18:56 -0500

    94 ajout HttpErreur

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,52 +1,52 @@
-const express = require('express');
+const express = require("express");
 
+const HttpErreur = require("../modeles/http-erreur");
 const router = express.Router();
 
 const PLACES = [
-    {
-      id: 'p1',
-      titre: 'Empire State Building',
-      description: 'Grosse bâtisse!',
-      location: {
-        lat: 40.7484474,
-        lng: -73.9871516
-      },
-      adresse: '20 W 34th St, New York, NY 10001',
-      createur: 'u1'
-    }
-  ];
+  {
+    id: "p1",
+    titre: "Empire State Building",
+    description: "Grosse bâtisse!",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    adresse: "20 W 34th St, New York, NY 10001",
+    createur: "u1",
+  },
+];
 
+router.get("/:placeId", (requete, reponse, next) => {
+  console.log(2);
+  const placeId = requete.params.placeId;
+  const place = PLACES.find((place) => {
+    return place.id === placeId;
+  });
 
-router.get('/:placeId', (requete, reponse, next) => {
-    console.log(2);
-    const placeId = requete.params.placeId;
-    const place = PLACES.find(place => {
-        return place.id === placeId;
-    })
+  if (!place) {
+    return next(HttpErreur("Aucune place trouvé pour l'id fourni", 404));
+  }
 
-    if(!place){
-        const errreur = new Error("Aucune place trouvé pour l'id fourni")
-        errreur.code = 404;
-        return next(errreur);
-     }
-
-    reponse.json({place}); //Revient à {place:place}
+  reponse.json({ place }); //Revient à {place:place}
 });
 
-router.get('/utilisateur/:utilisateurId', (requete, reponse, next) => {
-    const utilisateurId = requete.params.utilisateurId;
-    
-    const places = PLACES.find(place => {
-        return place.createur === utilisateurId;
-    })
-    
-    if(!places){
-        const errreur = new Error("Aucune place trouvée pour l'utilisateur fourni")
-        errreur.code = 404;
-        throw errreur
-     }
+router.get("/utilisateur/:utilisateurId", (requete, reponse, next) => {
+  const utilisateurId = requete.params.utilisateurId;
 
-    reponse.json({places});
+  const places = PLACES.find((place) => {
+    return place.createur === utilisateurId;
+  });
+
+  if (!places) {
+    const errreur = new HttpErreur(
+      "Aucune place trouvée pour l'utilisateur fourni",
+      404
+    );
+    throw errreur;
+  }
+
+  reponse.json({ places });
 });
 
 module.exports = router;
