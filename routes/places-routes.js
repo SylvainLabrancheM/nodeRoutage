c6a6f444d21a0393f0e7aee74bc9565e98bef0e4
Labranche <slabranche@cmontmorency.qc.ca>
2023-03-08 08:51:12 -0500

    91 route place

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,28 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (requete, reponse, next) => {
+const PLACES = [
+    {
+      id: 'p1',
+      titre: 'Empire State Building',
+      description: 'Grosse bâtisse!',
+      location: {
+        lat: 40.7484474,
+        lng: -73.9871516
+      },
+      adresse: '20 W 34th St, New York, NY 10001',
+      createur: 'u1'
+    }
+  ];
+
+
+router.get('/:placeId', (requete, reponse, next) => {
+    const placeId = requete.params.placeId;
+    const place = PLACES.find(place => {
+        return place.id === placeId;
+    })
     console.log("Requête GET dans places-routes")
-    reponse.json({message: "Ça fonctionne!"});
+    reponse.json({place}); //Revient à {place:place}
 });
 
 module.exports = router;
